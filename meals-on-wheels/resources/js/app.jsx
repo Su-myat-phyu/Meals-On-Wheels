15fdb2cf15776f691fa564ac1e71866167c887ae
Su Myat Phyu <sumyatphyu2422004@gmail.com>
2024-12-18 22:12:10 +0630

    Added aboutus and contactus page

--- meals-on-wheels/resources/js/app.jsx
+++ meals-on-wheels/resources/js/app.jsx
@@ -1,12 +1,25 @@
-import '../css/app.css'; // Import Tailwind CSS
-import React from 'react';
-import { createRoot } from 'react-dom/client';
+import '../css/app.css';
+import './bootstrap';
+
 import { createInertiaApp } from '@inertiajs/react';
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+import { createRoot } from 'react-dom/client';
+
+const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
 createInertiaApp({
-    resolve: (name) => import(`./Pages/${name}.jsx`),
+    title: (title) => `${title} - ${appName}`,
+    resolve: (name) =>
+        resolvePageComponent(
+            name === 'ContactUs'
+                ? './Pages/features/contactus/pages/ContactUsPage.jsx'
+                : `./Pages/${name}.jsx`,
+            import.meta.glob('./Pages/**/*.jsx'),
+        ),
+    
     setup({ el, App, props }) {
         const root = createRoot(el);
+
         root.render(<App {...props} />);
     },
 });
